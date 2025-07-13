8ca0fff8e2c3ec958f86d9594a6ecc9d2b14ef41
Jun Hayashi <jh@junnoMacBook-Air.local>
2025-07-13 11:51:24 -0300

    build

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,34 +1,22 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
-from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/accounts/', include('accounts.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # 本番はNginxなどが静的ファイルを配信すべき
+    pass
+
+# SPAのindex.htmlにフォールバックするルールは最後に置く（ただし静的ファイルは除く）
+from django.views.generic import TemplateView
+from django.urls import re_path
+
+urlpatterns += [
+    re_path(r'^(?!static/|api/|admin/|assets/).*$', TemplateView.as_view(template_name="index.html")),
+]
