14f4b2b2d6ef6655978cd02788237ef03cb0ec4c
Jun Hayashi <jh@junnoMacBook-Air.local>
2025-07-13 11:47:33 -0300

    build

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,12 +19,14 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.views.static import serve
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/accounts/', include('accounts.urls')),
-    re_path(r'^.*$', serve, kwargs={'path': 'index.html', 'document_root': settings.BASE_DIR / 'dist'}),
+    re_path(r'^.*$', TemplateView.as_view(template_name="index.html")),
 ]
 
 
