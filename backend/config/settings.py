811ff75cbb127234945f5e7d9688808bcfa81457
Jun Hayashi <jh@junnoMacBook-Air.local>
2025-07-12 23:27:43 -0300

    build

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -13,7 +13,14 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 
 
 # Quick-start development settings - unsuitable for production
